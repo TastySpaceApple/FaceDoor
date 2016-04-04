b256412efa4db092568ab820b1001b17d12293ba
unknown <idan.itzhaky@gmail.com>
2016-04-04 23:21:36 +0300

    added display window at fullscreen

--- trainer.py
+++ trainer.py
@@ -8,6 +8,7 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 
+
 print "======= Training =========="
 print "This will take a few images of you face to train on"
 facename = raw_input('What is your name? ')
@@ -21,15 +22,12 @@ except:
 
 imgindex = 0
 
-while imgindex < amount:
-    print "Get Ready"
-    counter = 3
-    while counter > 0:
-        print counter
-        counter-=1
-        time.sleep(1)
 
-    print "Taking picture"
+cv2.namedWindow("Video", cv2.WINDOW_NORMAL);
+cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+
+
+while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     
@@ -43,22 +41,25 @@ while imgindex < amount:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         face = gray[y: y + h, x: x + h]
-        print "Face size in image: %d x %d"  % (w, h)
+        #print "Face size in image: %d x %d"  % (w, h)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame, str("face"), (x+2, y+h-2), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
         break;
 
     # Display the resulting frame
-    # cv2.imshow('Video', frame)
+    cv2.imshow('ad', frame)
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
 
-    if face is not None:
-        imgindex += 1
-        filename = "%s.%d.png" % (facename, imgindex)
-        filepath = os.path.join("references", filename)
-        cv2.imwrite(filepath, face)
-        print ("Image %d Taken" % imgindex);
-    else:
-        print "No face was detected. Please try again"
+
+##    if face is not None:
+##        imgindex += 1
+##        filename = "%s.%d.png" % (facename, imgindex)
+##        filepath = os.path.join("references", filename)
+##        cv2.imwrite(filepath, face)
+##        print ("Image %d Taken" % imgindex);
+##    else:
+##        print "No face was detected. Please try again"
 
 # When everything is done, release the capture
 video_capture.release()
